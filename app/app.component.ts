ea0e8e7c885c556fd432ca2a34169c3c379a4e06
chandu2chandu1 <chandu2chandu1@gmail.com>
2016-12-22 10:53:11 +0530

    Repo added

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core'
+
+@Component({
+    selector: 'my-app',
+    template:'<h2>Welcome guys</h2>'
+})
+
+export class AppComponent{
+    
+}
---
+++ app/courses.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'courses',
+    template: '<h2>Hello, welcome to angular js 2</h2>'    
+})
+export class AppComponent{
+
+}
